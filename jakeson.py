84af88972461de73ed293263d8b983fc4f79cd34
Chad Estioco <chadestioco@gmail.com>
2021-06-04 05:08:36 +0800

    Move stuff around so we can accommodate the property queries.

--- jakeson.py
+++ jakeson.py
@@ -12,6 +12,24 @@ def is_blank(s):
             return False
     return True
 
+def readquired(schema_field, check=is_blank):
+    """
+    check is a function that, as long as true, would prompt the user for input.
+    """
+    required = input(f"{schema_field}: ")
+
+    while check(required):
+        required = input(f"{schema_field}: ")
+
+    return required
+
+def is_truthy(c):
+    if len(c) > 1:
+        raise ValueError(f"was expecting a single-char input, received {c}")
+
+    norm = c.lower()
+    return norm in "yt"
+
 def main():
     schema = {}
 
@@ -19,10 +37,7 @@ def main():
         schema[schema_field] = input(f"{schema_field}: ")
 
     def readpeatedly(schema_field):
-        schema[schema_field] = input(f"{schema_field}: ")
-
-        while is_blank(schema[schema_field]):
-            schema[schema_field] = input(f"${schema_field}: ")
+        schema[schema_field] = readquired(schema_field)
 
     print("First things first...")
     _schema = input(f"$schema ({SCHEMA_VER}): ")
@@ -32,6 +47,30 @@ def main():
     read("title")
     read("description")
     readpeatedly("type")
+
+    if schema["type"] == "object":
+        print("Let's get to the meat of things (Ctrl-C to stop)...")
+        schema["required"] = []
+        schema["properties"] = {}
+
+        while True:
+            try:
+                propname = readquired("property name")
+                description = readquired("description")
+                _type = readquired("type")
+                is_required = readquired("is required (Y/n)", lambda x: len(x) and x.lower() not in "ytnf")
+                if is_truthy(is_required):
+                    schema["required"].append(propname)
+
+                schema["properties"][propname] = {
+                    "description": description,
+                    "type": _type
+                }
+
+                print("---")
+            except KeyboardInterrupt:
+                print("\nWriting schema...")
+                break
 
     return schema
 
