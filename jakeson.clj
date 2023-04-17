1a1abc27732be729e2b12a1a829943d4c02bc238
Chad Estioco <chadestioco@gmail.com>
2023-04-17 18:53:19 +0200

    Untested brain dump.

--- jakeson.clj
+++ jakeson.clj
@@ -21,6 +21,16 @@
       (throw (RuntimeException. (str "was expecting single-char input, received " c)))
       (contains? FALSEY (.toLowerCase c))))
 
+(defn read-w-prompt [prompt]
+  (print prompt ": ")
+  (flush)
+  (read-line))
+
+(defn read-w-prompt [prompt default]
+  (print prompt "(" default "): ")
+  (flush)
+  (read-line))
+
 (defn read_validated [prompt check?]
   (print prompt)
   (flush)
@@ -40,7 +50,7 @@
         (join " " (cons "0:SKIP" choice-strings)))))
 
 ; Returns the index of the choice picked, or -1 if not required and none was chosen
-(defn read_choices [schema_field choices is_required]
+(defn read-choices [schema_field choices is_required]
   (let [prompt (clojure.string/join " " [schema_field (generate_choices_prompt choices is_required)])]
     (if is_required
         (Integer/parseInt (read_validated prompt
@@ -69,3 +79,57 @@
     (if (bool? truth-read)
         (truthy? truth-read)
         (truthy? default-val))))
+
+(defn propkey-check [propkey read-fn]
+  (if (= propkey "jakeson.STOP")
+      nil
+      (read-fn)))
+
+(declare read-sub-objs)
+(defn read-object-properties [obj-path running-props required-props pending-sub-objs]
+  (println "Define properties for " obj-path)
+  (println "Existing properties: " (keys running-props))
+  (let [propkey (readquired "property key")
+        description (propkey-check propkey #((read-w-prompt "description")))
+        required? (propkey-check propkey #((read-bool "required" false)))
+        _type (propkey-check propkey #((read-choices "type" TYPES true)))]
+    (cond
+      (and (= propkey "jakeson.STOP") (empty? pending-sub-objs)) running-props
+      (= propkey "jakeson.STOP") (read-sub-objs pending-sub-objs)
+      (= _type "object") (recur obj-path
+                                (assoc running-props propkey {"type" "object"})
+                                (if required? (cons propkey required-props) required-props)
+                                (cons propkey pending-sub-objs))
+      :else (recur obj-path
+                   (assoc running-props propkey "type" _type)
+                   (if required? (cons propkey required-props) required-props)
+                   pending-sub-objs))))
+
+; driver method
+(defn read-object-properties [obj-path]
+  (read-object-properties obj-path {} [] []))
+
+(defn read-sub-objs [parent-obj-path other-props pending-sub-objs]
+  (if (empty? (rest pending-sub-objs))
+      (assoc other-props (first pending-sub-objs)
+             (read-object-properties (join "." parent-obj-path (first pending-sub-objs)) [] []))
+      (recur parent-obj-path
+             (assoc other-props (first pending-sub-objs)
+                    (read-object-properties (join "." parent-obj-path (first pending-sub-objs)) [] []))
+             (rest pending-sub-objs))))
+
+(defn top-level-driver []
+  (let [schema (read-w-prompt "schema" SCHEMA_VER)
+        id (readquired "id")
+        title (readquired "title")
+        description (read-w-prompt "description")
+        _type (read-choices "type" TYPES true)]
+    (if (= _type "object")
+        (read-object-properties title)
+        {
+          "$schema" schema
+          "$id" id
+          "title" title
+          "description" description
+          "type" _type
+        })))
